0bd76fc2a1b083d2730c097fa8502b6275dfcf95
MihirMistry2 <mihirmistry82@gmail.com>
2024-02-12 07:16:54 +0530

    CSS was added to improve responsiveness on empty screens.

--- src/components/Empty/index.js
+++ src/components/Empty/index.js
@@ -13,18 +13,9 @@ export const Empty = ({ forwardedRef }) => {
             <Search ref={forwardedRef} />
             <div className="logo-container d-flex-column align-items justify-content">
                 <div className="logo-wrapper">
-                    <img
-                        id="logo-img"
-                        className="logo-img"
-                        src={LogoImg}
-                        alt="logo img"
-                        width="200px"
-                        height="200px"
-                    />
+                    <img id="logo-img" className="logo-img" src={LogoImg} alt="logo img" width="200px" height="200px" />
                 </div>
-                <div className="msg-wrapper">
-                    allow location access or search via city name.
-                </div>
+                <div className="msg-wrapper">allow location access or search via city name.</div>
             </div>
         </div>
     );
